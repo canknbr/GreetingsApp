48145e6c37336795f6863155af684c990c388d04
canknbr <cankanbur1@gmail.com>
2023-03-27 15:09:42 +0300

    Initial Commit

---
+++ Greatings/GreatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatingsApp.swift
+//  Greatings
+//
+//  Created by Can Kanbur on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GreatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
